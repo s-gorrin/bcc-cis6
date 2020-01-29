ae89795e554ba27dc16f524be51788d970c902e9
s-gorrin <36459991+s-gorrin@users.noreply.github.com>
2020-01-29 12:34:01 -0800

    further testing is needed for various inputs

--- major_assignment_1/problem2.cpp
+++ major_assignment_1/problem2.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-#include <math.h>
 
 using namespace std;
 
 int	main() {
 	// Seth Gorrin. 29 January 2020
-	// 
-	float due, rec, change;
-	int dollars, quarters, dimes, nickles, pennies;
+	// only works with correct input
+	// rounding is hard
+	float due, rec, raw_change;
+	int change, dollars, quarters, dimes, nickles, pennies;
 
 	dollars = 0;
 	quarters = 0;
@@ -20,40 +20,32 @@ int	main() {
 	cout << "how much did you pay? (format: x.yz) ";
 	cin >> rec;
 
-	change = rec - due;
-	cout << "change before rounding " << change << endl;
-	//PROBLEM IS HERE
-	//change = floorf(change * 100) / 100;
-	change = (int)((change + .005) * 100) / 100.0;
-	cout << "change after rounding " << change << endl;
+	raw_change = rec - due;
+	change = (int)(raw_change * 100);
 
-	if (change < 0) {
-		cout << "you have under paid by $" << change << endl;
+	if (raw_change < 0) {
+		cout << "you have under paid by $" << raw_change << endl;
 	}
 	else if (change == 0) {
 		cout << "you paid exact change" << endl;
 	}
 	else {
 		int temp, cents;
-		dollars = (int)change;
-		change -= (float)dollars;
-		cents = change * 100;
+		dollars = change / 100;
+		change -= (dollars * 100);
+		cents = change;
 
 		// get modulo, store it in temp, subtract it from change, divide by coin
 		temp = cents % 25;
 		quarters = (cents - temp) / 25;
-		cout << "temp after 25s " << temp << endl;
 
 		cents = temp;
 		temp = cents % 10;
 		dimes = (cents - temp) / 10;
-		cout << "temp after 10s " << temp << endl;
 
 		cents = temp;
 		temp = cents % 5;
 		nickles = (cents - temp) / 5;
-
-		cout << "temp after 5s " << temp << endl;
 
 		pennies = temp;
 	}
@@ -63,8 +55,3 @@ int	main() {
 
 	return 0;
 }
-
-
-// Write a program that directs a cashier how to give change.  
-// The program has two inputs: the amount due and the amount received from the customer.  
-// Display the dollars, quarters, dimes, nickels, and pennies that the customer should receive.
