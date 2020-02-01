e8961a4484fe2b34296768c614d52b114be3edc8
s-gorrin <36459991+s-gorrin@users.noreply.github.com>
2020-02-01 15:19:28 -0800

    Fix some float errors, clean, comment

--- major_assignment_1/problem2.cpp
+++ major_assignment_1/problem2.cpp
@@ -2,56 +2,60 @@
 
 using namespace std;
 
-int	main() {
-	// Seth Gorrin. 29 January 2020
-	// only works with correct input
-	// rounding is hard
-	float due, rec, raw_change;
-	int change, dollars, quarters, dimes, nickles, pennies;
+int main() {
+        // Seth Gorrin. 29 January 2020
+        float due, rec, raw_change;
+        int change, dollars, quarters, dimes, nickles, pennies;
 
-	dollars = 0;
-	quarters = 0;
-	dimes = 0;
-	nickles = 0;
-	pennies = 0;
+        // initialize zero values for ease of printing later
+        dollars = 0;
+        quarters = 0;
+        dimes = 0;
+        nickles = 0;
+        pennies = 0;
 
-	cout << "how much is due? (format: x.yz) ";
-	cin >> due;
-	cout << "how much did you pay? (format: x.yz) ";
-	cin >> rec;
+        // get user input
+        cout << "how much is due? (format: x.yz) ";
+        cin >> due;
+        cout << "how much did you pay? (format: x.yz) ";
+        cin >> rec;
 
-	raw_change = rec - due;
-	change = (int)(raw_change * 100);
+        // convert from float, try not to have rounding errors
+        rec *= 100;
+        due *= 100;
+        raw_change = (rec - due) / 100;
+        change = (int)(raw_change * 100); // get change as an int
 
-	if (raw_change < 0) {
-		cout << "you have under paid by $" << raw_change << endl;
-	}
-	else if (change == 0) {
-		cout << "you paid exact change" << endl;
-	}
-	else {
-		int temp, cents;
-		dollars = change / 100;
-		change -= (dollars * 100);
-		cents = change;
+        // get values
+        if (raw_change < 0) {
+                cout << "you have under paid by $" << (raw_change * -1) << endl;
+        }
+        else if (change == 0) {
+                cout << "you paid exact change" << endl;
+        }
+        else {
+                int temp, cents; // is this correct scoping?
+                dollars = change / 100;
+                change -= (dollars * 100);
+                cents = change;
 
-		// get modulo, store it in temp, subtract it from change, divide by coin
-		temp = cents % 25;
-		quarters = (cents - temp) / 25;
+                // get modulo, store it in temp, subtract it from cents, divide by coin
+                temp = cents % 25;
+                quarters = (cents - temp) / 25;
 
-		cents = temp;
-		temp = cents % 10;
-		dimes = (cents - temp) / 10;
+                cents = temp;
+                temp = cents % 10;
+                dimes = (cents - temp) / 10;
 
-		cents = temp;
-		temp = cents % 5;
-		nickles = (cents - temp) / 5;
+                cents = temp;
+                temp = cents % 5;
+                nickles = (cents - temp) / 5;
 
-		pennies = temp;
-	}
+                pennies = temp;
+        }
 
-	cout << "change due:" << endl;
-	cout << dollars << " dollars" << endl << quarters << " quarters" << endl << dimes << " dimes" << endl << nickles << " nickles" << endl << pennies << " pennies" << endl;
+        cout << "change due:" << endl;
+        cout << dollars << " dollars" << endl << quarters << " quarters" << endl << dimes << " dimes" << endl << nickles << " nickles" << endl << pennies << " pennies" << endl;
 
 	return 0;
 }
