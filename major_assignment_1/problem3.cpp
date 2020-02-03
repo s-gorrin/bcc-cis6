f456202a18651763c25c62d1d56d49e5e647b615
s-gorrin <36459991+s-gorrin@users.noreply.github.com>
2020-02-03 11:30:59 -0800

    Clean up header

--- major_assignment_1/problem3.cpp
+++ major_assignment_1/problem3.cpp
@@ -1,15 +1,17 @@
+/**
+ * File: problem3.cpp
+ * ----------
+ * creadted by: Seth Gorrin 2020-01-29
+ * invert a number (does not need to store it)
+ * for BCC CIS6 Spring 2020
+**/
+
 #include <iostream>
 #include <string>
 
 using namespace std;
 
 int flipper(string num, int n){
-        // I was trying to do something different
-        // which I thought would work and be clever
-        // but it turns out it didn't work
-        // becasue I understand neither recursion
-        // nor strings in C++
-        // but this seems to work, so here you go
         if(n >= 0){
                 cout << num[n];
                 return flipper(num, --n);
