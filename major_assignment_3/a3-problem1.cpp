b4a6826776e5c23dc148a9a70090eac7d7a8e38f
s-gorrin <36459991+s-gorrin@users.noreply.github.com>
2020-02-24 10:54:31 -0800

    Create a3-problem1.cpp

---
+++ major_assignment_3/a3-problem1.cpp
@@ -0,0 +1,57 @@
+/**
+ * Filename a3-problem1.cpp
+ * ----------
+ * created by: Seth Gorrin 2020-02-24
+ * Encrypt/decrypt a file with rot13
+ * No second file needed because it would be the same.
+ * for BCC CIS6 Spring 2020
+**/
+
+#include <fstream>
+#include <string>
+#include <ctype.h>
+
+using namespace std;
+
+char rot13(char c) {
+	if (islower(c)) {
+		if (c <= 'm')
+			return c += 13;
+		else
+			return c -= 13;
+	}
+
+	if (isupper(c)) {
+		if (c <= 'M')
+			return c += 13;
+		else
+			return c -= 13;
+	}
+
+	if (isdigit(c)) {
+		if (c <= '4')
+			return c += 5;
+		else
+			return c -= 5;
+	}
+	return c;
+}
+
+int main() {
+	ifstream fileIn("input.txt");
+	ofstream fileOut("output.txt");
+	string line;
+
+	if (fileIn.is_open() && fileOut.is_open()) {
+		while (getline(fileIn, line)) {
+			for (int i = 0; i < line.length(); i++) {
+				fileOut << rot13(line[i]);
+			}
+			fileOut << endl;
+		}
+		fileIn.close();
+		fileOut.close();
+	}
+
+	return 0;
+}
