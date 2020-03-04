76254517f3296e3ff2b4d66106ce596abbb847a2
Seth Gorrin <cymbae@Phoenix.local>
2020-03-04 08:12:55 -0800

    working on histogram algorithm

--- major_assignment_3/a3-problem3.cpp
+++ major_assignment_3/a3-problem3.cpp
@@ -76,20 +76,16 @@ int findBiggest(int counters[]) {
 
 // fill histogram with '#' marks based on counter values
 // SOMETHING IS WRONG WITH MY ALGORITHM HERE
-// I should be getting a ratio such that the tallest (in this case all)
-// bar is 50, and the rest are scaled based on 
-// bar of n height * ratio = number of hashes
-// bar of max height * ratio = 50
-// bar of 0.5max height * ration = 25
-// DO SOME MATH OR SOMETHING. FUCK
+// should never get a bar of height 51... check much taller bars
+// and see if the tallest is always that? (put more whitespace above)
 void makeBarGraph(int counters[], vector<vector<char> > &histogram) {
 	int ratio = findBiggest(counters) / MAX_BAR;
 
 	cout << ratio << endl;
 	for (int i = 0; i < C_SIZE; i++) {
 		for (int j = 0; j < BORDER_ALLOW; j++) {
-			if (j <= counters[i] * ratio / 100) { // if the current index is <=
-												  // the height of this bar
+			if (j <= counters[i] / ratio) {	// if the current index is <=
+											// the height of this bar
 				histogram.at(i).push_back('#');
 			}
 			else { // if this spot is taller than the bar height, make it a ' '
