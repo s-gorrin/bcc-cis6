1daf22e38312261652c68307de125e3a5a0a3903
Seth Gorrin <cymbae@Phoenix.local>
2020-04-18 23:31:10 -0700

    Started and finished problem 4

--- major_assignment_4/a4-problem4.cpp
+++ major_assignment_4/a4-problem4.cpp
@@ -1,8 +1,36 @@
 /**
  * Filename a3-problem4.cpp
  * ~~~~~~~~~~~~~~~~~~~~~~~~
- * created by: Seth Gorrin [DATE]
- * 
- * 
+ * created by: Seth Gorrin 18 April 2020
+ * implement Ackermann's Function
+ * very small numbers only
  * for BCC CIS6 Spring 2020
 **/
+
+#include <iostream>
+
+using namespace std;
+
+const int M = 3; // Probably best not to go above 3 on this one
+const int N = 13; // Tested up to 15, which seg faulted after 2 minutes
+
+// The Ackermann Function. I tried outputting the values as it ran,
+// But 3, 2 was so many lines I quickly decided time made more sense
+long ackermann(int m, int n) {
+	if (m == 0)
+		return n + 1;
+	if (n == 0)
+		return ackermann(m - 1, 1);
+
+	return ackermann(m - 1, ackermann(m, n - 1));
+}
+
+int main() {
+	cout << "No user input because I want " << 
+	   "lots of thinking to go into number choices." << endl
+	   << "Change values in consts. Carefully." << endl;
+
+	cout << ackermann(M, N) << endl;
+
+	return 0;
+}
